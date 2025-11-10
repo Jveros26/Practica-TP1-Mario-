4f04cbb9d4c96ab5aa9dd38be855d5c00796fb7b
Jveros26 <jveros@ucm.es>
2025-11-10 11:04:14 +0100

    Update ActionCommand.java

--- PR2/src/Tp1/control/commands/ActionCommand.java
+++ PR2/src/Tp1/control/commands/ActionCommand.java
@@ -29,7 +29,14 @@ public class ActionCommand extends AbstractCommand {
 	 @Override
 	 public Command parse(String[] commandWords) {
 		 if(commandWords.length<2) {
-			 return null;
+			 if(this.matchCommandName(commandWords[0])) {
+				 actionList.add(Action.RIGHT);
+				 return this;
+			 }
+			 else {
+				 return null;
+ 
+			 }
 		 }
 		 else {
 			 if(this.matchCommandName(commandWords[0])) {	//Veo si el primer argumento concuerda con la el nombre d ela clase
